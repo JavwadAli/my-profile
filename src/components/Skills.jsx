71b4985c9ad18cd5353f791ff19f3dc09af8dd6d
JavwadAli <javwadali786@gmail.com>
2023-06-17 18:49:30 +0530

    Project Completed Added all pages

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import Firebase from '../assets/firebase.png';
 import AWS from '../assets/aws.png';
-import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
 
@@ -18,8 +16,8 @@ const Skills = () => {
         {/* Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
-                <p className='py-4'>//These are the technologies I've worked with</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
+                <p className='py-4'>These are the technologies I've worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
